06fce76584fe5bdbfaeba442cd7dc8882c80ece2
taisei74 <taise297ugu@gmail.com>
2022-10-10 23:08:59 +0900

    失敗

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-    </head>
-    <body>
+@section('content')
+<div class='container'>
           <h1>予算以下に収まるようにランダムで3店舗紹介します</h1>
         <form action='/serch/random' method="POST">
         @csrf
@@ -51,7 +44,7 @@
          <input type="submit" value="送信"/>
          </form>
          </div>
+          <a href='/'>ホームに戻る</a>
          </div>
-         <a href='/'>ホームに戻る</a>
-    </body>
-</html>
+        </div>
+@endsection
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,26 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-    </head>
-    <body>
-        <!--<div>-->
-        <!--    <div class='yosan'>-->
-        <!--        <h2>[<a href='/serch'>予算でプランを調べる</a>]</h2>-->
-        <!--    </div>-->
-        <!--    <div class='zenbu'>-->
-        <!--        <h2>[<a href='/shop'>予算以下の店舗を調べる</a>]</h2>-->
-        <!--    </div>-->
-        <!--</div>-->
-        
-        
-        <div class='yosan'>
+@section('content')
+   
+    <div class='yosan'>
         <h1><a href='/serch'>検索をする</a></h1>
         </div>
         <div class='create'>
@@ -32,5 +14,4 @@
         <div>
             <h4><a href='/favorite/plan/index'>人気プランを一覧にする</a></h4>
         </div>
-    </body>
-</html>
+@endsection
